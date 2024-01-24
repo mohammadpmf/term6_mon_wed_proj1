5926bbbff7ac0225079047ace6fe16286b4c8ff5
soheila76 <ramezanzadeh.soheila76@gmail.com>
2024-01-24 20:33:58 +0330

    make model without author

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    STATUS_CHOICES = (
+        ('p', 'published'),
+        ('d', 'draft'),
+    )
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    # author = models.ForeignKey()
+    
