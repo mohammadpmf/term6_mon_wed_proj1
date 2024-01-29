9a2f39685ae51a819cacc44598364758607ec5eb
soheila76 <ramezanzadeh.soheila76@gmail.com>
2024-01-29 19:40:19 +0330

    for arshia

--- blog/models.py
+++ blog/models.py
@@ -10,5 +10,6 @@ class BlogPost(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
-    # author = models.ForeignKey()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
     
